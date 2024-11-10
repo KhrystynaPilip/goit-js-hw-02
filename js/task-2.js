3a72e7374e04b58235891f4fd7f4fbf06fd598ff
Khrystyna Pilip <tina.hristi@gmail.com>
2024-11-10 22:37:01 +0100

    Fix errors

--- js/task-2.js
+++ js/task-2.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
         return message.slice();
@@ -14,13 +13,3 @@ console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum
 console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
 console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
-=======
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
- };
-
- console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
->>>>>>> e2e97c0605f4fb3c071f50c3268711d9aee06d50
